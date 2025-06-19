bdb3477c435e10c6759dbf65465d40199a2cb786
KTakundwa <st10471537@vcconnect.edu.za>
2025-06-19 10:44:10 +0200

    Updates:
    I added the listing for the array and also activated the output string.

--- app/src/main/java/vcmsa/ci/practicumexamapp/ScreenTwo.kt
+++ app/src/main/java/vcmsa/ci/practicumexamapp/ScreenTwo.kt
@@ -2,6 +2,7 @@ package vcmsa.ci.practicumexamapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,8 +19,41 @@ class ScreenTwo : AppCompatActivity() {
             insets
         }
 
-       val btnReturnToMain = findViewById<Button>(R.id.btnReturnToMain)
+        val btnReturnToMain = findViewById<Button>(R.id.btnReturnToMain)
         val btnCalculate = findViewById<Button>(R.id.btnCalculate)
         val btnDisplay = findViewById<Button>(R.id.btnDisplay)
+        val lblOutputPlaylist = findViewById<TextView>(R.id.lblOutputPlaylist)
+        val lblOutputRating = findViewById<TextView>(R.id.lblOutputRating)
+
+        btnReturnToMain.setOnClickListener {
+            finish()
+        }
+        btnDisplay.setOnClickListener {
+            var outputString: String = ""
+            //for loop that runs 0 to the position
+            for (i in 0..arrayPosition) {
+                if (songRatings[i] >= 5) {
+                    outputString += "Song Title:" + songTitles[i] + "\n"
+                    outputString += "Artist Name: " + artistNames[i] + "\n"
+                    outputString += "Song Rating:" + songRatings[i].toString() + "\n"
+                    outputString += "Song Comments:" + songRatings[i] + "\n\n"
+                }
+                lblOutputPlaylist.text = outputString
+            }
+            arrayOf(1,2,3,4,5).average()
+
+            btnCalculate.setOnClickListener {
+                val averageRating
+            }
+
+
+
+
+        }
     }
 }
+
+
+
+
+
