3c1a97c48d9fd64acfe3fbebb0a6426d64dae6f3
KTakundwa <st10471537@vcconnect.edu.za>
2025-06-19 09:16:29 +0200

    Updates:
    I did my declarations for all the variables. Created a gloal parallel array at the top that has all of the values needed. I've Set my button, added log statements and comments. Added Toast and If statements for Error handling. Added a Next Button to Navigate to the next Screen. I lastly Added the exit button to shut down the app. The first Screen is now complete.

--- app/src/main/java/vcmsa/ci/practicumexamapp/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/practicumexamapp/MainActivity.kt
@@ -1,10 +1,21 @@
 package vcmsa.ci.practicumexamapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+var songTitles  =Array<String>(4){""}
+var  artistNames= Array<String> (4){""}
+var songRatings = Array<Int> (4){0}
+var songCommets = Array<String> (4) {""}
+var arrayPosition = 0
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +29,97 @@ class MainActivity : AppCompatActivity() {
         }
         // Student name: Kimberly Takundwa
         // Student Number: ST10471537
+
+        val btnAddToPlaylist = findViewById<Button>(R.id.btnAddToPlaylist)
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+        var lblArtistName = findViewById<EditText>(R.id.lblArtistName)
+        var lblRate = findViewById<EditText>(R.id.lblRate)
+        var lblSongTitle = findViewById<EditText>(R.id.lblSongTitle)
+        var lblComments = findViewById<EditText>(R.id.lblComments)
+        val TAG = "Playlist Manager App"
+
+        btnAddToPlaylist.setOnClickListener {
+            val songTitle: String = lblSongTitle.text.toString()
+            val artistName: String = lblArtistName.text.toString()
+            val songComment: String = lblComments.text.toString()
+            val songRating: String = lblRate.text.toString()
+
+            if (songTitle.isNullOrBlank()) {  //(IIE, 2025)
+                Toast.makeText(
+                    this,
+                    "Input cannot be left blank, please enter a valid input and try again.",
+                    Toast.LENGTH_SHORT //This toast message is for error handling
+                ).show()
+                Log.d(TAG, "The user did not enter a song title")
+                //If the input is invalid, exit out of the onclick method
+                return@setOnClickListener
+
+            }
+            if (songRating.isNullOrBlank()) {
+                Toast.makeText(
+                    this,
+                    "Input cannot be blank,enter the numbers between 1-5 and try again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d(TAG, "The user did not enter the song rating number")
+                return@setOnClickListener
+
+            } else if (!songRating.isDigitsOnly()) {
+                Toast.makeText(
+                    this,
+                    "Enter only digits here",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+            if (songComment.isNullOrBlank()) {
+                Toast.makeText(
+                    this,
+                    "Input cannot be blank, please enter a valid comment and try again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d(TAG, "The user did not enter a comment")
+                return@setOnClickListener
+
+            }
+            if (artistName.isNullOrBlank()) {
+                Toast.makeText(
+                    this,
+                    "Input cannot be blank, please enter a valid input and try again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.d(TAG, "The user did not enter the artist's name")
+                return@setOnClickListener
+
+            }
+            if (arrayPosition < 4) { //(IIE, 2025)
+                songTitles[arrayPosition] = songTitle
+                artistNames[arrayPosition] = artistName
+                songRatings[arrayPosition] = songRating.toInt()
+                songCommets[arrayPosition] = songComment
+                arrayPosition++
+                Toast.makeText(this, "Added to Playlist 🎶❤️!", Toast.LENGTH_SHORT)
+                    .show() //Toast Message to show that the Items were added
+                Log.d(
+                    TAG,
+                    "User has tried to add songs to full array:("
+                ) // Log Statement For Understanding of the Code
+
+
+            }
+        }
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, ScreenTwo::class.java)
+            startActivity(intent)
+            //Starting the Next Screen
+
+        }
+        //Setting teh exit Button to completely shut down the app
+        btnExit.setOnClickListener {
+            finishAffinity()
+
+        }
     }
 }
