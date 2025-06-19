915a23b51081e7831d8d98f7c249d3e358e099ca
KTakundwa <st10471537@vcconnect.edu.za>
2025-06-19 08:37:56 +0200

    Added buttons and background image to my main activity.

---
+++ app/src/main/java/vcmsa/ci/practicumexamapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package vcmsa.ci.practicumexamapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Student name: Kimberly Takundwa
+        // Student Number: ST10471537
+    }
+}
